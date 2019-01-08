36ac158e0bf75709dbff1afdb6732be48d6375cd
Eva-AlHindy <44072763+Eva-AlHindy@users.noreply.github.com>
2019-01-08 12:22:01 +0100

    Eva

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+function slidebar_open() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+
+function slidebar_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
