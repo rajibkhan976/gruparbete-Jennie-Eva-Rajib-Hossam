88a02b66b09efac49c51fe974d7218333394a415
unknown <rajibkhan976@gmail.com>
2019-01-18 14:20:21 +0100

    Rajib 2019-01-18 14:20

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+
   function slidebar_open() {
     document.getElementById("mySidebar").style.display = "block";
   }
@@ -6,20 +6,3 @@ $(document).ready(function(){
   function slidebar_close() {
     document.getElementById("mySidebar").style.display = "none";
   }
-  $('#snakeGame').click(function () {
-    var level1 = "html/level-1.html";
-    $(location).attr('href', level1);
-  });
-  $('#memoryGame').click(function () {
-    var level2 = "html/level-2.html";
-    $(location).attr('href', level2);
-  });
-  $('#iqGame').click(function () {
-    var level3 = "html/level-3_html.html";
-    $(location).attr('href', level3);
-  });
-  $('#dragonGame').click(function () {
-    var level4 = "html/level-4.html";
-    $(location).attr('href', level4);
-  });
-});
